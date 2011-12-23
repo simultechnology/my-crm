30f386a17b1f23789cf8e173f97c95c1c6a6aeb5
simultechnology <tissi0708@gmail.com>
2011-12-24 01:45:33 +0900

    changed database.yml for heroku

--- config/routes.rb
+++ config/routes.rb
@@ -65,6 +65,8 @@ Crm::Application.routes.draw do
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
 
+  root to: 'customers#new', as: 'customer'
+
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
